29fb78298f4bc841722c1945724c26886e92903a
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2024-03-03 21:43:24 +0530

    Create 08_math.cs

---
+++ Programs/08_math.cs
@@ -0,0 +1,9 @@
+
+
+Console.WriteLine("\nWelcome to learning about Math class in C#.\n");
+
+Console.WriteLine($"Maximum number between 5 and 10 : {Math.Max(5, 10)}");
+Console.WriteLine($"Minimum number between 5 and 10 : {Math.Min(5, 10)}");
+Console.WriteLine($"Square root of 64 : {Math.Sqrt(64)}");
+Console.WriteLine($"Absolute value of -4.7 : {Math.Abs(-4.7)}");
+Console.WriteLine($"Round number of 9.99 : {Math.Round(9.99)}");
