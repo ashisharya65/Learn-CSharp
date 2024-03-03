29384017f7283f76976dbd7aa64d23b073bbe092
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2024-03-03 21:40:32 +0530

    Create 02_variables.cs

---
+++ Programs/02_variables.cs
@@ -0,0 +1,8 @@
+
+// using datatype in front of the variable name
+string name = "Ashish Arya";
+Console.WriteLine($"'{name}' variable is of type '{name.GetType()}'.");
+
+// You can also use var keyword before the variable name
+var age = 33;
+Console.WriteLine($"My name is {name} and I am {age} years old.");
