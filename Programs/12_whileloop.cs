39f50059f00868635d440983ef37a0089ec8e8ab
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2024-03-03 21:45:03 +0530

    Create 12_whileloop.cs

---
+++ Programs/12_whileloop.cs
@@ -0,0 +1,19 @@
+
+Console.WriteLine("\nWelcome to learning about while loop in C#.\n");
+
+// while loop
+int i = 0;
+while (i <= 10)
+{
+    Console.WriteLine($"Iteration : {i}");
+    i++;
+}
+
+// do-while loop
+int j = 0;
+do
+{
+    Console.WriteLine(j);
+    j++;
+}
+while (j < 5);
